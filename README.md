a369b10fd253816a14c0ff03d1dc5093f96a712b
Daiki Yamanaka <daikitdaiki@gmail.com>
2013-07-22 18:27:20 +0900

    added README and test data

---
+++ README.md
@@ -0,0 +1,10 @@
+Canonical Correlation Analysis (CCA)
+=====================================================
+This is simple implementation of Canonical Correlation Analysis.
+
+### requirement
+1. OpenCV
+2. Eigen
+
+
+
--- cca.cpp
+++ cca.cpp
@@ -16,42 +16,25 @@ CCA::~CCA(){
 
 void CCA::calc(){
 
-	Sxx = X*X.t();
-	//std::cout << Sxx << std::endl;
-	std::cout << "Sxx.rows: " << Sxx.rows << " cols: " << Sxx.cols << std::endl;
+	Sxx = X*X.t();	
 	Sxy = X*Y.t();	
-	//std::cout << Sxy << std::endl;	
-	std::cout << "Sxy.rows: " << Sxy.rows << " cols: " << Sxy.cols << std::endl;	
 	Syx = Y*X.t();
-	//std::cout << Syx << std::endl;	
-	std::cout << "Syx.rows: " << Syx.rows << " cols: " << Syx.cols << std::endl;	
 	Syy = Y*Y.t();
-	//std::cout << Syy << std::endl;		
-	std::cout << "Syy.rows: " << Syy.rows << " cols: " << Syy.cols << std::endl;	
 
-	std::cout << "construct matrix S" << std::endl;
+	//std::cout << "construct matrix S" << std::endl;
 	cv::Mat Sb = Syy.inv()*Syx*Sxx.inv()*Sxy;
 	cv::Mat Sa = Sxx.inv()*Sxy*Syy.inv()*Syx;
 
-	std::cout << "SVD" << std::endl;
+	//std::cout << "SVD" << std::endl;
 	cv::SVD svda(Sa);
 	cv::SVD svdb(Sb);
 
-	A = svda.u;
-	std::cout << "A.rows: " << A.rows << " cols: " << A.cols << std::endl;
+	A = svda.u;	
 	B = svdb.u;		
-	std::cout << "B.rows: " << B.rows << " cols: " << B.cols << std::endl;	
-
-	std::cout << A << std::endl;
-	std::cout << B << std::endl;	
-
-	//std::cout << "A.rows: " << A.rows << " cols: " << A.cols << std::endl;	
-	//std::cout << "B.rows: " << B.rows << " cols: " << B.cols << std::endl;		
 }
 
 cv::Mat CCA::predict(cv::Mat x){
-	//std::cout << x.rows << " " << x.cols << std::endl;
-	return A.t()*x/B.at<float>(0, 0);
+	return A.t()*x*B.inv();
 }
 
 cv::Mat CCA::calc_center(cv::Mat &X){
